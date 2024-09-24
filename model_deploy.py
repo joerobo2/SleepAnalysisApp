1cfd8abef9454f857d86e03ab4244f75526133f5
Joseph Robinson <160297841+joerobo2@users.noreply.github.com>
2024-09-23 23:40:28 -0500

    Update model_deploy.py

--- model_deploy.py
+++ model_deploy.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-conda install scikit-learn
+from requirements import scikit-learn
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.ensemble import HistGradientBoostingRegressor
 
